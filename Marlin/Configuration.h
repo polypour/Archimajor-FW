7e9574799676afb987f9ef30708811999e659332
tonokip <tonokip@gmail.com>
2016-11-08 10:19:24 -0600

    fix max pos for taz4

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -397,9 +397,9 @@ const bool Z_PROBE_ENDSTOP_INVERTING = false; // set to true to invert the logic
 #define X_MIN_POS 0
 #define Y_MIN_POS 0
 #define Z_MIN_POS 0
-#define X_MAX_POS 200
-#define Y_MAX_POS 200
-#define Z_MAX_POS 200
+#define X_MAX_POS 290
+#define Y_MAX_POS 290
+#define Z_MAX_POS 251
 
 //===========================================================================
 //========================= Filament Runout Sensor ==========================
